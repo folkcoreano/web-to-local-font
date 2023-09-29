2a0907fe2e0c3e4211b7e1826bcb76e65472632f
folkcoreano <folkcoreano@outlook.com>
2023-09-28 23:04:10 -0300

    polimento e docu

--- README.md
+++ README.md
@@ -1,25 +1,45 @@
-# Web to Local Fonts
+# Web to Local Fonts!
 
-Essa é uma simples ferramente para fazer download de arquivos de fonte web originadas do Google Fonts e similares.
+> Essa é uma simples ferramenta para fazer download de arquivos de fonte web originadas do Google Fonts e similares.
 
-O uso é bastante simples, não precisa instalar.
+## Uso
 
-```bash
+> É bastante simples, não precisa instalar:
+
+```sh
+pnpm dlx web-to-local-font -u <url>
+bunx web-to-local-font -u <url>
 npx web-to-local-font -u <url>
 ```
 
-Por exemplo:
+> Mas se você quiser, pode!
 
-```bash
-npx web-to-local-font -u https://fonts.googleapis.com/css2?family=Roboto&display=swap
+```sh
+pnpm install -D web-to-local-font
+bun install -D web-to-local-font
+npm install -D web-to-local-font
 ```
 
-```bash
-pnpm dlx web-to-local-font -u https://fonts.googleapis.com/css2?family=Roboto&display=swap
+## Exemplos
+
+> É importante que você coloque a url da folha de estilo logo em seguida do parametro `-u`, por algum motivo. E a url agradece se for colocada entre aspas, duplas ou simples fica a gosto.
+
+```sh
+pnpm dlx web-to-local-font -u "https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+
+bunx web-to-local-font -u "https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+
+npx web-to-local-font -u "https://fonts.googleapis.com/css2?family=Roboto&display=swap"
 ```
 
-```bash
-bunx web-to-local-font -u https://fonts.googleapis.com/css2?family=Roboto&display=swap
+> Tudo será baixado em uma pasta chamada `fonts` por padrão, mas você pode mudar o caminho se desejar.
+
+```sh
+pnpm dlx web-to-local-font -f "src/assets/css" -u "https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+
+bunx web-to-local-font -f "src/assets/css" -u "https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+
+npx web-to-local-font -f "src/assets/css" -u "https://fonts.googleapis.com/css2?family=Roboto&display=swap"
 ```
 
-Esse comando irá baixar todas as fontes da folha de estilo na pasta /fonts/files, na raiz dessa pasta terá o arquivo fonts.css.
+> Com isso será criada uma pasta `files` contendo todos os arquivos de fontes, e ao lado dela o arquivo `fonts.css` indexando todas as fontes com suas especifícações em `@font-face`.
--- index.js
+++ index.js
@@ -1,91 +1,80 @@
 #!/usr/bin/env node
 
 import Downloader from "nodejs-file-downloader";
-import fs from "fs";
+import { readFile, writeFile } from "node:fs";
 import minimist from "minimist";
 import ora from "ora";
 
-const log = console.log;
-
-const argv = minimist(process.argv.slice(2));
-const url = argv.u;
-const dir = argv.f;
-
-async function read_sheet(sheet, folder = "fonts") {
-  const spinner = ora("starting... ").start();
-
+(async function read_sheet() {
   const a = performance.now();
+  const argv = minimist(process.argv.slice(2));
+  const url = argv.u;
+  const folder = argv.f || "fonts";
+  const spinner = ora("folha carregada... ").start();
   const download_sheet = new Downloader({
-    url: sheet,
+    url: url,
     directory: folder,
     fileName: "fonts.css",
     cloneFiles: false,
   });
   await download_sheet.download();
-
-  spinner.text = "stylesheet loaded...";
-
-  const sheet_file = fs.readFileSync(`${folder}/fonts.css`, {
-    encoding: "utf-8",
-  });
-
-  const faces = sheet_file.split("@font-face").slice(1);
-  const faces_mapped = [];
-  for (const face of faces) {
-    const src = face
-      .split("src: ")
-      .at(1)
-      .split("unicode-range")
-      .at(0)
-      .split(", ")
-      .map((url) => {
-        const meta_name = url
-          .split(" format")
-          .at(0)
-          .split("url(")
+  readFile(
+    `${folder}/fonts.css`,
+    { encoding: "utf-8" },
+    async (error, sheet_file) => {
+      const faces = sheet_file.split("@font-face").slice(1);
+      const faces_mapped = [];
+      for (const face of faces) {
+        const src = face
+          .split("src: ")
           .at(1)
-          .split(")")
-          .at(0);
-        return {
-          url: meta_name,
-          name: meta_name.split("/").at(-1),
-        };
+          .split("unicode-range")
+          .at(0)
+          .split(", ")
+          .map((url) => {
+            const meta_name = url
+              .split(" format")
+              .at(0)
+              .split("url(")
+              .at(1)
+              .split(")")
+              .at(0);
+            return {
+              url: meta_name,
+              name: meta_name.split("/").at(-1),
+            };
+          });
+        faces_mapped.push(...src);
+      }
+      spinner.text = `${faces_mapped.length} font files loaded...`;
+      let raw = sheet_file;
+      let n_urls = 0;
+      const downloads = [];
+      for (const url of faces_mapped) {
+        n_urls++;
+        downloads.push(
+          new Promise(async (res, rej) => {
+            const download = new Downloader({
+              url: url.url,
+              fileName: url.name,
+              directory: `${folder}/files`,
+              cloneFiles: false,
+            });
+            await download.download();
+            res();
+          })
+        );
+
+        raw = raw.replaceAll(url.url, `'files/${url.name}'`);
+
+        spinner.text = `baixando ${n_urls}/${faces_mapped.length}: ${url.name}... `;
+      }
+      await Promise.all(downloads);
+      writeFile(`${folder}/fonts.css`, raw, { encoding: "utf-8" }, () => {
+        spinner.succeed(
+          `tudo pronto em ${(performance.now() - a).toFixed(2)}ms!`
+        );
       });
-    faces_mapped.push(...src);
-  }
-
-  spinner.text = `${faces_mapped.length} font files loaded...`;
-
-  let raw = sheet_file;
-  let n_urls = 0;
-
-  const downloads = [];
-
-  for (const url of faces_mapped) {
-    n_urls++;
-    downloads.push(
-      new Promise(async (res, rej) => {
-        const download = new Downloader({
-          url: url.url,
-          fileName: url.name,
-          directory: `${folder}/files`,
-          cloneFiles: false,
-        });
-        await download.download();
-        res();
-      })
-    );
-
-    raw = raw.replaceAll(url.url, `'files/${url.name}'`);
-
-    spinner.text = `downloading ${n_urls}/${faces_mapped.length}: ${url.name}... `;
-  }
-
-  await Promise.all(downloads);
-
-  fs.writeFileSync(`${folder}/fonts.css`, raw, { encoding: "utf-8" });
-
-  spinner.succeed(`finished in ${(performance.now() - a).toFixed(2)}ms!`);
-}
-
-await read_sheet(url, dir);
+    }
+  );
+})();
